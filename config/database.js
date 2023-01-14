b9cef6cda82c307b379f93ed5d6ce8f22a88fe1e
Chiranjeev Vishnoi <66114276+Chiranjeev-droid@users.noreply.github.com>
2023-01-14 10:18:26 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,8 +1,6 @@
-import mongoose from "mongoose";
-//connecting Mongoose database
 export const connectDB = async (req, res) => {
   mongoose
     .connect(process.env.MONGO_URI)
     .then((con) => console.log(`Database Connected: ${con.connection.host}`))
-    .catch((err) => console.log(err));
+    .catch((err) => console.log("error", err));
 };
