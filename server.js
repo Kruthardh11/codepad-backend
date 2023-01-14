deebd1454a342a3b5f39b4b2c34a6a19954aed21
Chiranjeev Vishnoi <66114276+Chiranjeev-droid@users.noreply.github.com>
2023-01-14 12:57:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,7 @@ import app from "./app.js";
 import { connectDB } from "./config/database.js";
 
 connectDB();
-app.get("/", (req, res, next) => {
-  res.status(200).json({
-    success: true,
-    message: "Working",
-  });
-});
+
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Server is working on port: ${process.env.PORT}`);
 });
