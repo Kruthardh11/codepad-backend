09b290aaf516b6e2cc29874b98f11bcaff7f41ff
Chiranjeev-droid <somu209e@gmail.com>
2023-01-14 12:31:59 +0530

    cors

--- server.js
+++ server.js
@@ -1,12 +1,5 @@
-import express from "express";
-import bodyParser from "body-parser";
 import app from "./app.js";
 import { connectDB } from "./config/database.js";
-import cors from "cors";
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors());
-
-app.use(express.json());
 
 connectDB();
 app.get("/", (req, res, next) => {
@@ -15,7 +8,6 @@ app.get("/", (req, res, next) => {
     message: "Working",
   });
 });
-
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Server is working on port: ${process.env.PORT}`);
 });
